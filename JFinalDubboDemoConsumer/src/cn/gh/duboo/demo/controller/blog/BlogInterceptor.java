8b23363c7a82437df1910f99b6c7b42f0e73794d
zhaoyz <18958003903@189.cn>
2015-04-30 23:30:13 +0800

    11

--- JFinalDubboDemoConsumer/src/cn/gh/duboo/demo/controller/blog/BlogInterceptor.java
+++ JFinalDubboDemoConsumer/src/cn/gh/duboo/demo/controller/blog/BlogInterceptor.java
@@ -11,6 +11,6 @@ public class BlogInterceptor implements Interceptor {
 	public void intercept(ActionInvocation ai) {
 		System.out.println("Before invoking " + ai.getActionKey());
 		ai.invoke();
-		System.out.println("After invoking 1111" + ai.getActionKey());
+		System.out.println("After invoking " + ai.getActionKey());
 	}
 }
